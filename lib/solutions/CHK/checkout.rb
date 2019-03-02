297070f9ceecc8093df69707fe157ae54e7bf822
sbx_user1059 <sbx_user1059@ip-10-129-88-30.eu-west-2.compute.internal>
2019-03-02 11:41:47 +0000

    Sat Mar 02 11:41:47 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,8 +1,23 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
+  SKUS = {
+    'A' => 50,
+    'B' => 30,
+    'C' => 20,
+    'D' => 15
+  }
 
   def checkout(skus)
     raise 'Not implemented'
   end
 
 end
+
+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |
+
--- spec/CHK/chk_spec.rb
+++ spec/CHK/chk_spec.rb
@@ -6,8 +6,27 @@ Logging.logger.root.appenders = Logging.appenders.stdout
 require_solution 'CHK'
 
 RSpec.describe Checkout do
-  it 'test_checkout' do
+  it 'returns -1 for invalid input' do
     checkout = Checkout.new
     expect(checkout.checkout(1)).to eq -1
   end
+
+  it 'returns 50 for item A' do
+    checkout = Checkout.new
+    expect(checkout.checkout('A')).to eq 50
+  end
+
+  it 'returns 115 for itesm ABCD' do
+    checkout = Checkout.new
+    expect(checkout.checkout('ABCD')).to eq 115
+  end
 end
+
+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |
+
