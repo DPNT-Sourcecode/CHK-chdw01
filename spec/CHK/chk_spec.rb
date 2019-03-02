b09dcf511b26b1ce97bcbbc3e8853eb9d83ae342
sbx_user1059 <sbx_user1059@ip-10-129-88-30.eu-west-2.compute.internal>
2019-03-02 11:53:29 +0000

    Sat Mar 02 11:53:29 UTC 2019

--- spec/CHK/chk_spec.rb
+++ spec/CHK/chk_spec.rb
@@ -8,17 +8,17 @@ require_solution 'CHK'
 RSpec.describe Checkout do
   it 'returns -1 for invalid input' do
     checkout = Checkout.new
-    expect(checkout.checkout(1)).to eq -1
+    expect(checkout.checkout('')).to eq 0
   end
 
   it 'string contains non capital chars' do
     checkout = Checkout.new
-    expect(checkout.checkout('a')).to eq -1
+    expect(checkout.checkout("ABCa")).to eq -1
   end
 
   it 'string contains non capital chars' do
     checkout = Checkout.new
-    expect(checkout.checkout('-')).to eq -1
+    expect(checkout.checkout("A-A")).to eq -1
   end
 
   it 'returns 50 for item A' do
@@ -30,7 +30,27 @@ RSpec.describe Checkout do
     checkout = Checkout.new
     expect(checkout.checkout('ABCD')).to eq 115
   end
+
+  it 'returns 115 for itesm ABCD' do
+    checkout = Checkout.new
+    expect(checkout.checkout("AAA")).to eq 130
+  end
 end
+
+
+INFO  AuditStream : id = CHK_R1_014, req = checkout("AAA"), resp = 150
+INFO  AuditStream : id = CHK_R1_015, req = checkout("AAAA"), resp = 200
+INFO  AuditStream : id = CHK_R1_016, req = checkout("AAAAA"), resp = 250
+INFO  AuditStream : id = CHK_R1_017, req = checkout("AAAAAA"), resp = 300
+INFO  AuditStream : id = CHK_R1_018, req = checkout("B"), resp = 30
+INFO  AuditStream : id = CHK_R1_019, req = checkout("BB"), resp = 60
+INFO  AuditStream : id = CHK_R1_020, req = checkout("BBB"), resp = 90
+INFO  AuditStream : id = CHK_R1_021, req = checkout("BBBB"), resp = 120
+INFO  AuditStream : id = CHK_R1_022, req = checkout("ABCDABCD"), resp = 230
+INFO  AuditStream : id = CHK_R1_023, req = checkout("BABDDCAC"), resp = 230
+INFO  AuditStream : id = CHK_R1_024, req = checkout("AAABB"), resp = 210
+INFO  AuditStream : id = CHK_R1_001, req = checkout("ABCDCBAABCABBAAA"), resp = 575
+
 
 
 # | Item | Price | Special offers |
@@ -39,5 +59,6 @@ end
 # | B    | 30    | 2B for 45      |
 # | C    | 20    |                |
 # | D    | 15    |
+
 
 
