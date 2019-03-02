f2c5182cb7aa4e13f93133b5bb3534048edad005
sbx_user1059 <sbx_user1059@ip-10-129-88-30.eu-west-2.compute.internal>
2019-03-02 12:38:29 +0000

    Sat Mar 02 12:38:29 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -7,13 +7,32 @@ class Checkout
     'D' => 15
   }
 
+  PROMOS = {
+    'AAA' = 130,
+    'BB' = 45
+  }
+
   def checkout(skus)
     return -1 unless !skus.match(/[^A-Z]/)
     return 0 if skus.empty?
 
-    skus.each_char.map do |sku|
-      SKUS.fetch(sku)
-    end.reduce(:+)
+    total = 0
+
+    a_count = skus.count('A')
+    b_count = skus.count('B')
+
+    PROMOS.each do |promo, discount|
+      ordered_skus.coun
+      if ordered_skus.include?(promo)
+        total += discount
+        ordered_skus.sub!(promo, '')
+      e
+    end
+
+
+    step 1 count all the letters.
+
+    step 2 apply the rule
   end
 end
 
@@ -24,3 +43,4 @@ end
 # | B    | 30    | 2B for 45      |
 # | C    | 20    |                |
 # | D    | 15    |
+
