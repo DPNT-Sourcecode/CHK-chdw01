3033c8b67a7cfbb7efef1b58d76d057bbc3ae9ec
sbx_user1059 <sbx_user1059@ip-10-129-88-30.eu-west-2.compute.internal>
2019-03-02 12:50:29 +0000

    Sat Mar 02 12:50:29 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -25,24 +25,20 @@ class Checkout
     end
 
     sku_counts.map do |sku, count|
+      if sku == 'A'
+        a_promos = count / 3
+        a_normal = count % 3
+
+        PROMOS['AAA'] * a_promos + SKUS[sku] * a_normal
+      elsif sku == 'B'
+        b_promos = count / 2
+        b_normal = count % 2
+        PROMOS['BB'] * b_promos + SKUS[sku] * b_normal
+      else
+        SKUS[sku] * count
+      end
 
     end.reduce(:+)
-
-    a_count = skus.count('A')
-    a_promos = a_count / 3
-    a_normal = a_count % 3
-
-    total += PROMOS['AAA'] * a_promos
-    total += SKUS['A'] * a_normal
-
-    b_count = skus.count('B')
-    b_promos = b_count / 2
-    b_normal = b_count % 2
-
-    total += PROMOS['BB'] * b_promos
-    total += SKUS['B'] * b_normal
-
-    total
   end
 end
 
@@ -53,6 +49,7 @@ end
 # | B    | 30    | 2B for 45      |
 # | C    | 20    |                |
 # | D    | 15    |
+
 
 
 
